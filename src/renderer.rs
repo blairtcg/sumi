8079870fd2f2274eadda59a699905210b49a41ad
seth <117851610+Sethispr@users.noreply.github.com>
2026-07-11 21:08:40 -0700

    refactor: CardRenderer and update stats handling

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,17 +1,14 @@
 pub mod cache;
 pub mod canvas;
-pub mod encoding;
+pub mod encoder;
 pub mod error;
 pub mod pixels;
 pub mod print;
 
 use std::{
     num::NonZero,
-    path::PathBuf,
-    sync::{
-        atomic::{AtomicU64, Ordering},
-        Arc,
-    },
+    path::Path,
+    sync::Arc,
     thread,
     time::{Duration, Instant},
 };
@@ -23,64 +20,48 @@ use error::RenderError;
 use print::init_font;
 use tokio::{sync::Semaphore, task, time::timeout, try_join};
 
+use crate::stats::AppStats;
+
 const TIMEOUT_SECONDS: u64 = 10;
-
-/// simple counter to keep track of how sumi is doing.
-#[derive(Default, Debug)]
-pub struct RequestStats {
-    pub total_requests: AtomicU64,
-    pub failed_requests: AtomicU64,
-}
-
-impl RequestStats {
-    /// saves details of a single request after it finishes
-    /// this updates our running totals safely across multiple threads.
-    pub fn record(&self, did_fail: bool) {
-        self.total_requests.fetch_add(1, Ordering::Relaxed);
-        if did_fail {
-            self.failed_requests.fetch_add(1, Ordering::Relaxed);
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct CardRenderer {
     pub card_cache: CardCache,
-    pub stats: RequestStats,
     pub start_time: Instant,
+    pub stats: AppStats,
     cpu_semaphore: Arc<Semaphore>,
     total_permits: usize,
 }
 
 impl CardRenderer {
-    pub fn new(cards_directory: impl Into<PathBuf>) -> Result<Self, RenderError> {
+    pub fn new(cards_directory: impl AsRef<Path>) -> Result<Self, RenderError> {
         let cores = thread::available_parallelism().map_or(4, NonZero::get);
-        log::info!("sumi found {cores} cpu cores");
+        tracing::info!("sumi woke up with [{cores} cpu cores]");
         init_font();
 
         Ok(Self {
-            card_cache: CardCache::new(cards_directory.into())?,
-            stats: RequestStats::default(),
+            card_cache: CardCache::new(cards_directory.as_ref())?,
             start_time: Instant::now(),
+            stats: AppStats::default(),
             cpu_semaphore: Arc::new(Semaphore::new(cores)),
             total_permits: cores,
         })
     }
 
-    /// wait for all background workers to finish
+    // wait for all background workers to finish
     pub async fn wait_for_tasks_to_finish(&self) {
         let active_tasks =
             self.total_permits.saturating_sub(self.cpu_semaphore.available_permits());
         if active_tasks > 0 {
-            log::info!("waiting for {active_tasks} tasks to drain...");
+            tracing::info!("finishing {} active tasks..", active_tasks);
         }
         let _ = self.cpu_semaphore.acquire_many(self.total_permits as u32).await;
-        log::info!("all tasks cleared!");
+        tracing::info!("all active tasks finished!");
     }
 
-    /// creates the final image.
-    /// if an image cant render your drop in 5 seconds, Too bad!
-    /// in blair-go side your cooldown wont get used. users can just try dropping again.
+    // creates the final image.
+    // if an image cant render your drop in 5 seconds, Too bad!
+    // in blair-go side your cooldown wont get used. users can just try dropping again.
     pub async fn render_drop(
         &self,
         left_card_name: &str,
@@ -91,11 +72,11 @@ impl CardRenderer {
         let render_future = async {
             let start_fetch = Instant::now();
             let (left_card, right_card) = try_join!(
-                self.card_cache.get_card(left_card_name),
-                self.card_cache.get_card(right_card_name)
+                self.card_cache.get(left_card_name),
+                self.card_cache.get(right_card_name)
             )?;
             let fetch_elapsed = start_fetch.elapsed();
-            log::debug!("fetching cards took {:.3}ms", fetch_elapsed.as_secs_f64() * 1000.0);
+            tracing::debug!("fetching cards took {:.3}ms", fetch_elapsed.as_secs_f64() * 1000.0);
 
             // need to acquire an owned permit so we can move it inside the blocking thread.
             // if the request times out, the thread still holds the permit until it finishes
@@ -104,7 +85,7 @@ impl CardRenderer {
                 .clone()
                 .acquire_owned()
                 .await
-                .expect("cpu semaphore closed unexpectedly");
+                .map_err(|_| RenderError::Internal("cpu semaphore died".to_string()))?;
 
             // move the heavy image work to a background thread
             let result = task::spawn_blocking(move || {
