44f47c59d0e160664cbb226a50fcc1522c77926c
seth <117851610+Sethispr@users.noreply.github.com>
2026-07-11 20:58:05 -0700

    chore: tweak encoder settings

--- src/renderer/encoding.rs
+++ src/renderer/encoding.rs
@@ -1,46 +1,48 @@
 use bytes::Bytes;
-use webpx::{EncoderConfig, Preset, Unstoppable};
+use webpx::{AlphaFilter, EncoderConfig, ImageHint, Preset, Unstoppable};
 
-use crate::renderer::{error::RenderError, pixels::RawCardImage};
+use crate::renderer::error::RenderError;
 
 const WEBP_QUALITY: f32 = 85.0;
-const WEBP_SPEED: i32 = 0;
-const WEBP_ALPHA_QUALITY: i32 = 80;
-const WEBP_THREAD_LEVEL: i32 = 1;
-const WEBP_SEGMENTS: i32 = 1;
+const WEBP_SPEED: u8 = 0;
+const WEBP_ALPHA_QUALITY: u8 = 85;
+const WEBP_THREAD_LEVEL: u8 = 0;
+const WEBP_SEGMENTS: u8 = 1;
 
-/// we take raw pixels of two diff cards from moka cache
+/// we take raw pixels of two diff cards from cache
 /// paste it into canvas and draw print nums
 /// but because of that huge amount of alpha pixels and bigger image dimension
 /// now if we send that pixels data over to discord, lets just say the file size would be 2 MB
 /// so we would suffer two things, our side and the users side of having to load that shit
-/// this is why we do reencoding back to a "new" compressed .webp giving us ~500 KB final file
+/// this is why we do reencoding back to a "new" compressed .webp giving us ~400 KB final file
 /// the point of using jpeg would be pointless as we need alpha support
 /// the point of skipping alpha compression in webpx is also pointless - similar reason
 /// using lossless is also not worth, although encoding takes 100ms instead of our current 350ms
 /// it is uncompressed and our drop image dimension is huge so again file size would be 2 MB - bad.
-pub fn encode_webp(image: &RawCardImage) -> Result<Bytes, RenderError> {
-    let width = image.size.width;
-    let height = image.size.height;
-    let pixel_data = &image.pixels;
-
+pub fn encode_webp(width: u32, height: u32, pixel_data: &[u8]) -> Result<Bytes, RenderError> {
     // we keep encoding on one thread so we avoid slowing down the server.
     // this is by far the only overhead we have, it'll take ~100ms per img.
     // see all other options at https://crates.io/crates/webpx
     let settings = EncoderConfig::new()
         // preset needs more testing to check which one is best for our cards
         .preset(Preset::Picture)
+        .hint(ImageHint::Picture)
         .quality(WEBP_QUALITY)
-        .method(WEBP_SPEED as u8)
-        .thread_level(WEBP_THREAD_LEVEL as u8)
+        .method(WEBP_SPEED)
+        .thread_level(WEBP_THREAD_LEVEL)
+        // see line 19 on why
         .alpha_compression(true)
-        .alpha_quality(WEBP_ALPHA_QUALITY as u8)
-        .low_memory(true)
+        .alpha_quality(WEBP_ALPHA_QUALITY)
+        // none filter maximizes alpha encoding speed
+        .alpha_filter(AlphaFilter::None)
+        .low_memory(false)
         .pass(1)
-        .sns_strength(0)
-        .filter_strength(0)
         .exact(false)
-        .segments(WEBP_SEGMENTS as u8);
+        // 3 = 8 partitions, which theoretically should unlock parallel decoding for discord users
+        // though i dont really know what goes on inside discord as they do their own weird shit
+        // but more partitions essentially means more file size which is bad for our I/O
+        // .partitions(3)
+        .segments(WEBP_SEGMENTS);
 
     let webp_data: Vec<u8> = settings
         .encode_rgba(pixel_data, width, height, Unstoppable)
