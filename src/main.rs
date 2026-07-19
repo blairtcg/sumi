20d0f5d00c7483395396ae3fbde652d3f73dafd0
Clarinep <konovbaraputra@gmail.com>
2026-07-19 18:10:49 +0700

    fix: switch to metrics module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod config;
 mod error;
 mod logger;
+mod metrics;
 mod renderer;
 mod routes;
-mod stats;
 
 use std::{error::Error, future::pending, net::SocketAddr, panic, sync::Arc, time::Duration};
 
@@ -103,7 +103,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     if timeout(Duration::from_secs(10), state.wait_for_tasks_to_finish()).await.is_ok() {
         tracing::info!("sumi is sleeping.. zZz");
     } else {
-        tracing::error!("sumi refused to sleep in time..\n      pulling the blanket anyway!");
+        tracing::error!("sumi refused to sleep in time.. pulling the blanket anyway!");
     }
 
     Ok(())
